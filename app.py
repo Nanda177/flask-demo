a134e413732b9210474523eaf6a351e847fc3b66
Nanda177 <nandakbr2001@gmail.com>
2025-11-12 14:50:55 +0530

    update ci yal file

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app=Flask(__name__)
 @app.route('/')
 def home():
     hostname = socket.gethostname()
-    return f"Hello from Flask!<br>Handled by Pod: {hostname}"
+    return f"Hello from Flask!<br>Handled by POD: {hostname}"
 
 if __name__=='__main__':
     app.run(host='0.0.0.0', port=5000)
